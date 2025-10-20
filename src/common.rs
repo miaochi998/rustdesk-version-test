bbad261b199bc2bd1fe8bf2b1a3642c9e9494689
miaochi998 <15027036@qq.com>
2025-10-20 11:56:51 +0800

    Test version 1.4.3 with patches - Server: yc.bonnei.com

--- src/common.rs
+++ src/common.rs
@@ -899,6 +899,9 @@ pub fn is_modifier(evt: &KeyEvent) -> bool {
 }
 
 pub fn check_software_update() {
+    // 🎯 拦截点2：禁用UI手动更新检查
+    log::info!("Software update check disabled by custom build");
+    return;
     if is_custom_client() {
         return;
     }
@@ -1024,7 +1027,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://yc.bonnei.com:21114".to_owned()
 }
 
 #[inline]
